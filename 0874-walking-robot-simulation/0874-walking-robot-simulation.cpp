e8f877557590137736df1f6a859e9ec8eaa0a9fe
Emilly Almeida <47991400+emillydalmeida@users.noreply.github.com>
2026-09-02 11:07:49 -0300

    Time: 275 ms (14.96%), Space: 133 MB (26.8%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        int dx[4] = {0, 1, 0, -1};
+        int dy[4] = {1, 0, -1, 0};
+
+        unordered_set<string> obs;
+        for (auto& o : obstacles) {
+            obs.insert(to_string(o[0]) + "," + to_string(o[1]));
+        }
+
+        int x = 0, y = 0;
+        int dir = 0;
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -2) {
+                dir = (dir + 3) % 4;
+            } 
+            else if (cmd == -1) {
+                dir = (dir + 1) % 4;
+            } 
+            else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+                    
+                    string key = to_string(nx) + "," + to_string(ny);
+                    if (obs.count(key)) {
+                        break;
+                    }
+                    
+                    x = nx;
+                    y = ny;
+                    
+                    maxDist = max(maxDist, x*x + y*y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
+};
